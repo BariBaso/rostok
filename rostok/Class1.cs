0364dd6218fb5fc0675c2a3e6a78eca7dde3dd95
BariBaso <kissmarci41@gmail.com>
2022-03-29 11:23:01 +0200

    Add project files.

---
+++ rostok/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rostok
+{
+    class Class1
+    {
+        public string MegNev { get; set; }
+        public string Kategoria { get; set; }
+        public string Egyseg { get; set; }
+        public string Rost { get; set; }
+
+
+        public Class1(string sor)
+        {
+            string[] s = sor.Split(';');
+
+            MegNev = s[0];
+            Kategoria = s[1];
+            Egyseg = s[2];
+            Rost = s[3];
+        }
+    }
+}
---
+++ rostok/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace rostok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Class1> lista = new List<Class1>();
+            foreach (var i in File.ReadAllLines("rostok.txt").Skip(1))
+            {
+                lista.Add(new Class1(i));
+            }
+
+            Console.WriteLine($"3.feladat:  Az élelmiszerek száma: {lista.Count}");
+            Console.WriteLine($"4.feladat: nem 100g-s egység: {lista.Where(x => x.Egyseg != "100g").Count()} ");
+            Console.WriteLine($"5.feladat: friss gümölcsök átlagos rost tartalma: {lista.Where(x => x.Egyseg == "100g").Sum(x => x.Rost).}");
+            
+
+
+
+
+        }
+    }
+}
